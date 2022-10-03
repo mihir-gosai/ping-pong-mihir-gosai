650913cd2a9b1139581d7f276150cf7f1ade8ec7
mihir-gosai <114927611+mihir-gosai@users.noreply.github.com>
2022-10-03 14:06:56 +0100

    ping pong

---
+++ Test/src/main/java/com/example/Controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/ping")
+	public String index() {
+		return "pong!";
+	}
+
+}
